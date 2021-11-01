e6dd1a28409f07828f71ceff1b3d6b73d06cfd19
Clint Gunter <clint@clint.io>
2021-11-01 13:59:46 -0500

    Get product list from API displaying on page

--- products-app/src/App.js
+++ products-app/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
+const API_URL = 'https://products-api-01.herokuapp.com/api/products';
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  // Fetch API data on page load
+  useEffect(() => {
+    const getProducts = async () => {
+      const resp = await axios.get(API_URL);
+      setProducts(resp.data);
+    }
+    getProducts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      {/* Header */}
+      <header>
+        <h1>ProductsApp</h1>
       </header>
+
+      <main>
+
+        {/* Search Box */}
+        <form>
+          <input type="text" name="Search" placeholder="Search" />
+        </form>
+
+        {/* Product Grid */}
+        <ul className="product-grid">
+          {products.map((product) => (
+          <li>
+            <img src={product.imgURL} width="100" alt={product.name} />
+            <h3>{product.name}</h3>
+            <span>{product.price}</span>
+          </li>
+          ))}
+        </ul>
+
+      </main>
+
     </div>
   );
 }
